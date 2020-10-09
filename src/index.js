b40b1481744ad3e5aff52c729a5ddd2d478fd094
dekasins <dekasins@gmail.com>
2020-10-09 12:29:42 +0300

    feat: test how  it works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // feature "lucky chance 50/50"
-  return false;
+  // let arr = str.split('');
+  // let ctr = 0;
+  //       arr.forEach(elem => {
+  //     switch(elem){
+  //       case '(': ctr++;
+  //       break;
+  //       case ')': ctr++;
+  //       break;
+  //       case '{': ctr+=2;break;
+  //       case '}': ctr-=2;break;
+  //       case '[': ctr+=3;break;
+  //       case ']': ctr+=3;break;
+
+  //     }
+  //   })
+  //   return ctr == 0;
+
+  
+    return true;
 }
