9a6bbafba221ae27bd22f4f36aaebd1a4c216aa0
dekasins <dekasins@gmail.com>
2020-10-14 22:40:49 +0300

    fix: solved

--- src/index.js
+++ src/index.js
@@ -8,32 +8,44 @@ module.exports = function check(str, bracketsConfig) {
        for (let i = 0; i < str.length; i++) {
          let letter = arr[i];
          for (let j = 0 ; j < configArr.length; j++){
+             if (configArr[j][0] != configArr[j][1]){
+             
            if (letter == configArr[j][0]){
              newArr[i] = j+1
-//               console.log(newArr[i]);
+               
+//               
+               
            } else if (letter == configArr[j][1]){
             newArr[i] = (j+1) * (-1);
-//               console.log(newArr[i]);
+               
+//               
            } else {
-//             return false;
+//             
            }
+         } else if ( (configArr[j][0] == configArr[j][1]) && letter == configArr[j][0] ) {
+             newArr[i] = (j+1)*100;
          }
+             }
          
        }
         
-        console.log(newArr);
+        // console.log(newArr);
         
         for ( let i = 0; i< newArr.length; i++){
-            if(newArr[i]*(-1) != stack[stack.length-1]){
+            
+            if (newArr[i] == stack[stack.length-1] && newArr[i] >= 100 ){
+                stack.pop();
+            } else if(newArr[i]*(-1) != stack[stack.length-1]){
                 stack.push(newArr[i]);
                 
             } else {
-              if (newArr[i] > 0){
-                return false;
-            }
+                if (newArr[i] > 0){
+                    return false;
+                }
                 stack.pop();
             }
         }
   
     return stack.length == 0 ? true : false;
 }
+
