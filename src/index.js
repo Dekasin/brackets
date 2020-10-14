2b84d3503ba9a4d63e166e019abab198e04e5833
dekasins <dekasins@gmail.com>
2020-10-14 22:03:58 +0300

    fix: with different brackets

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // feature "lucky chance 50/50"
-  // let arr = str.split('');
-  // let ctr = 0;
-  //       arr.forEach(elem => {
-  //     switch(elem){
-  //       case '(': ctr++;
-  //       break;
-  //       case ')': ctr++;
-  //       break;
-  //       case '{': ctr+=2;break;
-  //       case '}': ctr-=2;break;
-  //       case '[': ctr+=3;break;
-  //       case ']': ctr+=3;break;
-
-  //     }
-  //   })
-  //   return ctr == 0;
-
   
-    return true;
+  let arr = str.split(''),
+      configArr = bracketsConfig;
+  let newArr = [],
+      stack =[];
+
+       for (let i = 0; i < str.length; i++) {
+         let letter = arr[i];
+         for (let j = 0 ; j < configArr.length; j++){
+           if (letter == configArr[j][0]){
+             newArr[i] = j+1
+//               console.log(newArr[i]);
+           } else if (letter == configArr[j][1]){
+            newArr[i] = (j+1) * (-1);
+//               console.log(newArr[i]);
+           } else {
+//             return false;
+           }
+         }
+         
+       }
+        
+        console.log(newArr);
+        
+        for ( let i = 0; i< newArr.length; i++){
+            if(newArr[i]*(-1) != stack[stack.length-1]){
+                stack.push(newArr[i]);
+                
+            } else {
+              if (newArr[i] > 0){
+                return false;
+            }
+                stack.pop();
+            }
+        }
+  
+    return stack.length == 0 ? true : false;
 }
